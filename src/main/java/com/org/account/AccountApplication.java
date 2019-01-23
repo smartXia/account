ed34876c2d600c2cf5d1808bc1af233d71c8c827
“xiapeifu” <xiapeifu@corp-ci.com>
2019-01-23 09:42:06 +0800

    spriingboot 最稳定的一个版本仅供使用，报错的原因是因为相关的pom.xml引用错误

--- src/main/java/com/org/account/AccountApplication.java
+++ src/main/java/com/org/account/AccountApplication.java
@@ -1,11 +1,10 @@
 package com.org.account;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.org.account.mapper")//将项目中对应的mapper类的路径加进来就可以了
+//@MapperScan("com.org.account.mapper")//将项目中对应的mapper类的路径加进来就可以了
 public class AccountApplication {
 
     public static void main(String[] args) {
